e983dc48faa6f223262ade6b4c89223af93d43de
Liu WanJin <jinguo1129@gmail.com>
2020-02-08 17:24:20 +0800

    Update

--- src/janus.mobile.js
+++ src/janus.mobile.js
@@ -41,22 +41,22 @@ import {
   
   // Screensharing Chrome Extension ID
   Janus.extensionId = "hapfgfdkleiggjjpfpenajgdnfckjpaj";
-  // Janus.isExtensionEnabled = function() {
-  //     if(window.navigator.userAgent.match('Chrome')) {
-  //         var chromever = parseInt(window.navigator.userAgent.match(/Chrome\/(.*) /)[1], 10);
-  //         var maxver = 33;
-  //         if(window.navigator.userAgent.match('Linux'))
-  //             maxver = 35;    // "known" crash in chrome 34 and 35 on linux
-  //         if(chromever >= 26 && chromever <= maxver) {
-  //             // Older versions of Chrome don't support this extension-based approach, so lie
-  //             return true;
-  //         }
-  //         return (document.getElementById('janus-extension-installed') !== null);
-  //     } else {
-  //         // Firefox of others, no need for the extension (but this doesn't mean it will work)
-  //         return true;
-  //     }
-  // };
+   Janus.isExtensionEnabled = function() {
+       if(window.navigator.userAgent.match('Chrome')) {
+           var chromever = parseInt(window.navigator.userAgent.match(/Chrome\/(.*) /)[1], 10);
+           var maxver = 33;
+           if(window.navigator.userAgent.match('Linux'))
+               maxver = 35;    // "known" crash in chrome 34 and 35 on linux
+           if(chromever >= 26 && chromever <= maxver) {
+               // Older versions of Chrome don't support this extension-based approach, so lie
+               return true;
+           }
+           return (document.getElementById('janus-extension-installed') !== null);
+       } else {
+           // Firefox of others, no need for the extension (but this doesn't mean it will work)
+           return true;
+       }
+   };
   
   Janus.noop = function() {};
   
